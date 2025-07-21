59e594cbe224bf0ae8d8ece6f8556efc2f644bb8
mrdkprj <rockfield.d.m@gmail.com>
2025-07-21 17:07:22 +0900

    No VipsApp

--- src/lib.rs
+++ src/lib.rs
@@ -17,68 +17,71 @@ pub mod utils;
 pub mod voption;
 
 use error::Error;
-use std::ffi::*;
-
 pub use image::*;
-
+use std::ffi::*;
 pub type Result<T> = std::result::Result<T, error::Error>;
 
-pub struct VipsApp;
+pub struct Vips;
 
 /// That's the main type of this crate. Use it to initialize the system
-impl VipsApp {
-    /// default constructor of a VIpsApp instance which will disable memory leak debugging
-    pub fn default(name: &str) -> Result<VipsApp> {
-        init(
-            name, false,
-        )?;
-        Ok(VipsApp)
+impl Vips {
+    pub fn init(name: &str, detect_leak: bool) -> Result<()> {
+        let cstring = utils::new_c_string(name);
+        if let Ok(c_name) = cstring {
+            let res = unsafe { bindings::vips_init(c_name.as_ptr()) };
+            let result = if res == 0 {
+                Ok(())
+            } else {
+                Err(Error::InitializationError("Failed to init libvips"))
+            };
+            unsafe {
+                if detect_leak {
+                    bindings::vips_leak_set(1);
+                };
+            }
+            result
+        } else {
+            Err(Error::InitializationError("Failed to convert rust string to C string"))
+        }
     }
 
-    /// new instance of VipsApp takes the application name and a flag indicating if the library should debug memory leak (good for testing purposes)
-    pub fn new(name: &str, detect_leak: bool) -> Result<VipsApp> {
-        init(
-            name,
-            detect_leak,
-        )?;
-        Ok(VipsApp)
-    }
-
-    pub fn progress_set(&self, flag: bool) {
+    pub fn progress_set(flag: bool) {
         unsafe {
             bindings::vips_progress_set(if flag { 1 } else { 0 });
         }
     }
 
-    pub fn get_disc_threshold(&self) -> u64 {
+    pub fn get_disc_threshold() -> u64 {
         unsafe { bindings::vips_get_disc_threshold() }
     }
 
-    pub fn version_string(&self) -> Result<&str> {
+    pub fn version_string() -> Result<String> {
         unsafe {
             let version = CStr::from_ptr(bindings::vips_version_string());
-            version
+            let version_str = version
                 .to_str()
-                .map_err(|_| Error::InitializationError("Error initializing string"))
+                .map_err(|_| Error::InitializationError("Error initializing string"))?;
+            Ok(version_str.to_string())
         }
     }
 
-    pub fn thread_shutdown(&self) {
+    pub fn thread_shutdown() {
         unsafe {
             bindings::vips_thread_shutdown();
         }
     }
 
-    pub fn error_buffer(&self) -> Result<&str> {
+    pub fn error_buffer() -> Result<String> {
         unsafe {
             let buffer = CStr::from_ptr(bindings::vips_error_buffer());
-            buffer
+            let buffer_str = buffer
                 .to_str()
-                .map_err(|_| Error::InitializationError("Error initializing string"))
+                .map_err(|_| Error::InitializationError("Error initializing string"))?;
+            Ok(buffer_str.to_string())
         }
     }
 
-    pub fn error(&self, domain: &str, error: &str) -> Result<()> {
+    pub fn error(domain: &str, error: &str) -> Result<()> {
         unsafe {
             let c_str_error = utils::new_c_string(error)?;
             let c_str_domain = utils::new_c_string(domain)?;
@@ -90,7 +93,7 @@ impl VipsApp {
         }
     }
 
-    pub fn error_system(&self, code: i32, domain: &str, error: &str) -> Result<()> {
+    pub fn error_system(code: i32, domain: &str, error: &str) -> Result<()> {
         unsafe {
             let c_str_error = utils::new_c_string(error)?;
             let c_str_domain = utils::new_c_string(domain)?;
@@ -103,138 +106,116 @@ impl VipsApp {
         }
     }
 
-    pub fn freeze_error_buffer(&self) {
+    pub fn freeze_error_buffer() {
         unsafe {
             bindings::vips_error_freeze();
         }
     }
 
-    pub fn error_clear(&self) {
+    pub fn error_clear() {
         unsafe {
             bindings::vips_error_clear();
         }
     }
 
-    pub fn error_thaw(&self) {
+    pub fn error_thaw() {
         unsafe {
             bindings::vips_error_thaw();
         }
     }
 
-    pub fn error_exit(&self, error: &str) -> Result<()> {
+    pub fn error_exit(error: &str) -> Result<()> {
         unsafe {
             let c_str_error = utils::new_c_string(error)?;
             bindings::vips_error_exit(c_str_error.as_ptr());
         }
     }
 
-    pub fn cache_print(&self) {
+    pub fn cache_print() {
         unsafe {
             bindings::vips_cache_print();
         }
     }
 
-    pub fn cache_set_max(&self, max: i32) {
+    pub fn cache_set_max(max: i32) {
         unsafe {
             bindings::vips_cache_set_max(max);
         }
     }
 
-    pub fn cache_set_max_mem(&self, max: u64) {
+    pub fn cache_set_max_mem(max: u64) {
         unsafe {
             bindings::vips_cache_set_max_mem(max);
         }
     }
 
-    pub fn cache_get_max(&self) -> i32 {
+    pub fn cache_get_max() -> i32 {
         unsafe { bindings::vips_cache_get_max() }
     }
 
-    pub fn cache_get_max_mem(&self) -> u64 {
+    pub fn cache_get_max_mem() -> u64 {
         unsafe { bindings::vips_cache_get_max_mem() }
     }
 
-    pub fn cache_get_size(&self) -> i32 {
+    pub fn cache_get_size() -> i32 {
         unsafe { bindings::vips_cache_get_size() }
     }
 
-    pub fn cache_set_max_files(&self, max: i32) {
+    pub fn cache_set_max_files(max: i32) {
         unsafe {
             bindings::vips_cache_set_max_files(max);
         }
     }
 
-    pub fn cache_get_max_files(&self) -> i32 {
+    pub fn cache_get_max_files() -> i32 {
         unsafe { bindings::vips_cache_get_max_files() }
     }
 
-    pub fn vips_cache_set_dump(&self, flag: bool) {
+    pub fn vips_cache_set_dump(flag: bool) {
         unsafe {
             bindings::vips_cache_set_dump(if flag { 1 } else { 0 });
         }
     }
 
-    pub fn vips_cache_set_trace(&self, flag: bool) {
+    pub fn vips_cache_set_trace(flag: bool) {
         unsafe {
             bindings::vips_cache_set_trace(if flag { 1 } else { 0 });
         }
     }
 
     /// set the number of worker threads for vips to operate
-    pub fn concurrency_set(&self, max: i32) {
+    pub fn concurrency_set(max: i32) {
         unsafe {
             bindings::vips_concurrency_set(max);
         }
     }
 
     /// get the number of worker threads that vips is operating
-    pub fn concurrency_get(&self) -> i32 {
+    pub fn concurrency_get() -> i32 {
         unsafe { bindings::vips_concurrency_get() }
     }
 
-    pub fn tracked_get_mem(&self) -> u64 {
+    pub fn tracked_get_mem() -> u64 {
         unsafe { bindings::vips_tracked_get_mem() }
     }
 
-    pub fn tracked_get_mem_highwater(&self) -> u64 {
+    pub fn tracked_get_mem_highwater() -> u64 {
         unsafe { bindings::vips_tracked_get_mem_highwater() }
     }
 
-    pub fn tracked_get_allocs(&self) -> i32 {
+    pub fn tracked_get_allocs() -> i32 {
         unsafe { bindings::vips_tracked_get_allocs() }
     }
 
-    pub fn pipe_read_limit_set(&self, limit: i64) {
+    pub fn pipe_read_limit_set(limit: i64) {
         unsafe {
             bindings::vips_pipe_read_limit_set(limit);
         }
     }
-}
 
-impl Drop for VipsApp {
-    fn drop(&mut self) {
+    pub fn shutdown() {
         unsafe {
             bindings::vips_shutdown();
         }
-    }
-}
-
-fn init(name: &str, detect_leak: bool) -> Result<i32> {
-    let cstring = utils::new_c_string(name);
-    if let Ok(c_name) = cstring {
-        let res = unsafe { bindings::vips_init(c_name.as_ptr()) };
-        let result = if res == 0 {
-            Ok(res)
-        } else {
-            Err(Error::InitializationError("Failed to init libvips"))
-        };
-        unsafe {
-            if detect_leak {
-                bindings::vips_leak_set(1);
-            };
-        }
-        result
-    } else {
-        Err(Error::InitializationError("Failed to convert rust string to C string"))
     }
 }
